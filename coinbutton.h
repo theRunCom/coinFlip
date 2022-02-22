3e618caf3c23a47998e390f1c245b3b6d1c8504e
theRunCom <49315766+theRunCom@users.noreply.github.com>
2022-02-22 14:06:16 +0800

    Update coinbutton.h

--- coinbutton.h
+++ coinbutton.h
@@ -14,10 +14,8 @@ public:
     int stat() const;
     void setStat(int stat);
 
-    // setStat带动画函数
     void setStatWithAnimation(int stat);
 
-    // 硬币翻转函数
     void flip();
 
 signals:
@@ -26,11 +24,8 @@ protected:
     void paintEvent(QPaintEvent *ev);
 
 private:
-    // 硬币状态，0表示银币，1表示金币
     int mStat;
-    // 记录当前动画帧数
     int mFrame;
-    // 定时器
     QTimer mTimer;
 };
 
