c39d3145c3d7846178941cb7766ba030d712e475
theRunCom <49315766+theRunCom@users.noreply.github.com>
2022-02-22 14:14:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-
-    // 开始场景
+    
     StartScene sc;
     sc.show();
-
-    // 选择场景
-//    SelectScene sc;
-//    sc.show();
-
+    
     return a.exec();
 }
