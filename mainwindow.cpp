771f6fa9d3c100d56fabafd683db5f3063bffc90
theRunCom <49315766+theRunCom@users.noreply.github.com>
2022-02-22 14:14:47 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,11 +9,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // 窗口标题
     this->setWindowTitle("翻金币");
-    // 设置窗口的图标
+
     this->setWindowIcon(QIcon(":/res/Coin0001.png"));
-    // 设置窗口固定大小
+
     this->setFixedSize(320, 588);
 }
 
@@ -24,7 +23,6 @@ MainWindow::~MainWindow()
 
 void MainWindow::paintEvent(QPaintEvent *event)
 {
-    // 绘制背景图片
     QPainter painter(this);
     QPixmap pix(":/res/MenuSceneBg.png");
     painter.drawPixmap(0, 0, this->width(), this->height(), pix);
